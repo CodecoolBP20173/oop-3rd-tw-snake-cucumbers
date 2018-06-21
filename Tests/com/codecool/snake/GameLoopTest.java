40d7955a33096b8dd5839849e43cb637c111b3ad
J-Abel <farinae.subactae@gmail.com>
2018-06-21 11:05:40 +0200

    Created the first two Test classes

---
+++ Tests/com/codecool/snake/GameLoopTest.java
@@ -0,0 +1,12 @@
+package com.codecool.snake;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameLoopTest {
+
+    @Test
+    void handle() {
+    }
+}
---
+++ Tests/com/codecool/snake/GameTest.java
@@ -0,0 +1,18 @@
+package com.codecool.snake;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void startTest() {
+    }
+
+    @Test
+    void initializeGameTest() {
+        Game game = new Game();
+
+    }
+}
